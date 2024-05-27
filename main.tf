084cae187e69bb71bb3d115e260286c33194e66e
anildash1234 <168274843+anildash1234@users.noreply.github.com>
2024-05-27 18:06:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "dev" {
+resource "aws_instance" "priya" {
     ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.micro"
     tags = {
